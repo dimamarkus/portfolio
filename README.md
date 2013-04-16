3a1cdbe17d1288f78f23d6c03f86e48baf046cc7
Dima Markus <dmi3000@gmail.com>
2013-04-16 18:20:03 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#My personal Web Page
+
+It is going to have 
+
+- information about me
+- examples of my work
+- a contact section
+- a blog
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'cf97622f43c6ada98b38db5c179e9cd05baae9e9b06b0f10235a7a7bd5ea73c9a1b397de9c45f7fe1425a71f5d2a3197d20fcf1f7a3c960c8224481c8ddd6b83'
